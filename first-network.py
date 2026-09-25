e34eb4073bc728a722b5a18f06aef9a0711b71b3
Rowan Evans <raevans2009@gmail.com>
2026-09-25 01:21:43 -0400

    multi-node / neuron layer, multiple weights all same inputs

--- first-network.py
+++ first-network.py
@@ -1,7 +1,15 @@
-inputs = [1, 2, 3]
+inputs = [1, 2, 3, 2.5]
 
-weights = [2.1, 3.4, 5.8, -0.5]
+weights1 = [2.1, 3.4, 5.8, -0.5]
+weights2 = [1.0, 0.3, -0.8, 0.1]
+weights3 = [0.9, -0.4, -0.6, 0.8]
 
-bias = 2
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
-output = weights[0]*inputs[0] + weights[1]*inputs[1] + weights[2]*inputs[2] + bias
+output = [weights1[0]*inputs[0] + weights1[1]*inputs[1] + weights1[2]*inputs[2] + bias1,
+          weights2[0]*inputs[0] + weights2[1]*inputs[1] + weights2[2]*inputs[2] + bias2,
+          weights3[0]*inputs[0] + weights3[1]*inputs[1] + weights3[2]*inputs[2] + bias3]
+
+print(output)
